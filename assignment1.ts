67650b9a40ca12e4f525bd824ac27122b3e6ffa8
vaibhav more <98808183+mvaibhav131@users.noreply.github.com>
2022-05-17 22:10:05 +0530

    Add files via upload

--- assignment1.ts
+++ assignment1.ts
@@ -7,8 +7,69 @@ salary:number;
 }
 
 
+var data:Users[]=[
+    {
+    "id": 1,
+    "name": "Ludwig O'Hoolahan",
+    "age": 95,
+    "salary": 5619
+  },
+   {
+    "id": 2,
+    "name": "Dyan Irce",
+    "age": 13,
+    "salary": 5932
+  }, 
+  {
+    "id": 3,
+    "name": "Thibaut Killiner",
+    "age": 16,
+    "salary": 4068
+  }, 
+  {
+    "id": 4,
+    "name": "Gretta Benedettini",
+    "age": 60,
+    "salary": 7633
+  },
+   {
+    "id": 5,
+    "name": "Natasha Ceschi",
+    "age": 83,
+    "salary": 9098
+  }, 
+  {
+    "id": 6,
+    "name": "Judy MacCroary",
+    "age": 78,
+    "salary": 14869
+  },
+   {
+    "id": 7,
+    "name": "Curr McGaffey",
+    "age": 94,
+    "salary": 38188
+  }, 
+  {
+    "id": 8,
+    "name": "Pennie Corwin",
+    "age": 21,
+    "salary": 73623
+  }, 
+  {
+    "id": 9,
+    "name": "Toddy Balhatchet",
+    "age": 76,
+    "salary": 3789
+  }, 
+  {
+    "id": 10,
+    "name": "Ethelyn Daen",
+    "age": 91,
+    "salary": 1677
+  }]
 
-var sort=(arr:[Users])=>{
+  function Sort(arr:Users[],by:keyof Users){
     for(var i=0;i<arr.length;i++){
         for(var j=i+1;j<arr.length;j++){
             if(arr[i]>arr[j]){
@@ -19,15 +80,4 @@ var sort=(arr:[Users])=>{
      console.log(arr)
 }
 
-var user:Users={
-    id:1,
-    age:44,
-    name:"sudhir",
-    salary:40000,
-};
-var user:Users={
-    id:2,
-    age:34,
-    name:"salman",
-    salary:17000,
-};
+Sort(data,"age")
